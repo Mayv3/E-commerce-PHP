c7b3bb3789a146b4ae1513bca65317f03b71fcb4
Nico :D <nicopereyra855@gmail.com>
2024-06-13 17:22:12 -0300

    Correccion de bug en la ruta del icono del logo

--- admin/index.php
+++ admin/index.php
@@ -39,7 +39,7 @@ if (!isset($routes[$view])) {
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container px-4 px-lg-5">
                 <img class="img-fluid logo-cubo" src="../img/logo-cubo.png" alt="Qubo indumentaria icono">
-                <a class="navbar-brand " href="index.php?section=home">Qubo Indumentaria</a>
+                <a class="navbar-brand " href="index.php?section=dashboard">Qubo Indumentaria</a>
                 <button class="navbar-toggler bg-secondary" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation"><span
@@ -47,11 +47,11 @@ if (!isset($routes[$view])) {
                 <div class="collapse navbar-collapse " id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                         <li class="nav-item"><a class="nav-link active" aria-current="page"
-                                href="index.php?section=login">Iniciar sesion</a></li>
-                        <li class="nav-item"><a class="nav-link active" aria-current="page"
                                 href="index.php?section=dashboard">Inicio</a></li>
                         <li class="nav-item"><a class="nav-link active" aria-current="page"
                                 href="index.php?section=products">Productos</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page"
+                                href="index.php?section=login">Iniciar sesion</a></li>
                     </ul>
                 </div>
             </div>
