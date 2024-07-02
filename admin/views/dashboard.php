80e0daa3d992993b6cc52b6001157147264585b0
Nico :D <nicopereyra855@gmail.com>
2024-07-02 17:50:03 -0300

    2

--- admin/views/dashboard.php
+++ admin/views/dashboard.php
@@ -5,14 +5,12 @@ require_once __DIR__ . '/../../models/Authentication.php';
 
 $user_email = (new Authentication)->get_session_user();
 ?>
-
-<main class="container">
-    <section>
-        <h1 class="mb-3">Panel de administrador de <span
-                class="text-info"><?php echo $user_email->get_user_email() ?></span> </h1>
-        <p class="text-dark">Te damos la bienvenida al panel de administracion. Aqui encontraras info</p>
-        <div>
-                <a class="btn btn-primary border-0" href="index.php?section=products">Ir a Productos</a>
-        </div>
-    </section>
-</main>
+<section class="container text-center">
+    <h1 class="mb-3">Panel de administrador
+        <span class="text-info fs-5"><?php echo $user_email->get_user_email() ?></span>
+    </h1>
+    <p class="text-dark">Te damos la bienvenida al panel de administracion. Aqui encontraras info</p>
+    <div>
+        <a class="btn btn-primary border-0" href="index.php?section=products">Ir a Productos</a>
+    </div>
+</section>
